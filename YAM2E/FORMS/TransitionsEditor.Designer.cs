2d8ec7cfa567d353c54f6887d756bfe42e6c8f7c
ConConner <egal3d@gmail.com>
2022-04-26 01:34:51 +0200

    Transition Editor progress
    Transition Editor can now view all Transitions. No editing though

--- YAM2E/FORMS/TransitionsEditor.Designer.cs
+++ YAM2E/FORMS/TransitionsEditor.Designer.cs
@@ -29,14 +29,20 @@ namespace YAM2E.FORMS
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Fadeout");
             this.grp_tred_selection = new System.Windows.Forms.GroupBox();
             this.lbl_tred_transition_selection = new System.Windows.Forms.Label();
             this.cbb_tred_transition_selection = new System.Windows.Forms.ComboBox();
             this.grp_tred_transition = new System.Windows.Forms.GroupBox();
             this.tre_tred_transition_tree = new System.Windows.Forms.TreeView();
+            this.grp_tred_edits = new System.Windows.Forms.GroupBox();
+            this.sts_tred_status_strip = new System.Windows.Forms.StatusStrip();
+            this.lbl_tred_transition_length = new System.Windows.Forms.ToolStripStatusLabel();
+            this.txt_tred_transition_data = new System.Windows.Forms.TextBox();
+            this.grp_tred_raw_data = new System.Windows.Forms.GroupBox();
             this.grp_tred_selection.SuspendLayout();
             this.grp_tred_transition.SuspendLayout();
+            this.sts_tred_status_strip.SuspendLayout();
+            this.grp_tred_raw_data.SuspendLayout();
             this.SuspendLayout();
             // 
             // grp_tred_selection
@@ -45,7 +51,7 @@ namespace YAM2E.FORMS
             this.grp_tred_selection.Controls.Add(this.cbb_tred_transition_selection);
             this.grp_tred_selection.Location = new System.Drawing.Point(13, 13);
             this.grp_tred_selection.Name = "grp_tred_selection";
-            this.grp_tred_selection.Size = new System.Drawing.Size(124, 415);
+            this.grp_tred_selection.Size = new System.Drawing.Size(125, 58);
             this.grp_tred_selection.TabIndex = 0;
             this.grp_tred_selection.TabStop = false;
             this.grp_tred_selection.Text = "Selection";
@@ -66,13 +72,14 @@ namespace YAM2E.FORMS
             this.cbb_tred_transition_selection.Name = "cbb_tred_transition_selection";
             this.cbb_tred_transition_selection.Size = new System.Drawing.Size(45, 23);
             this.cbb_tred_transition_selection.TabIndex = 0;
+            this.cbb_tred_transition_selection.SelectedIndexChanged += new System.EventHandler(this.cbb_tred_transition_selection_SelectedIndexChanged);
             // 
             // grp_tred_transition
             // 
             this.grp_tred_transition.Controls.Add(this.tre_tred_transition_tree);
             this.grp_tred_transition.Location = new System.Drawing.Point(144, 13);
             this.grp_tred_transition.Name = "grp_tred_transition";
-            this.grp_tred_transition.Size = new System.Drawing.Size(701, 415);
+            this.grp_tred_transition.Size = new System.Drawing.Size(320, 415);
             this.grp_tred_transition.TabIndex = 1;
             this.grp_tred_transition.TabStop = false;
             this.grp_tred_transition.Text = "Transition";
@@ -81,21 +88,64 @@ namespace YAM2E.FORMS
             // 
             this.tre_tred_transition_tree.Location = new System.Drawing.Point(7, 22);
             this.tre_tred_transition_tree.Name = "tre_tred_transition_tree";
-            treeNode1.BackColor = System.Drawing.Color.Silver;
-            treeNode1.Name = "Test";
-            treeNode1.Text = "Fadeout";
-            treeNode1.ToolTipText = "This fades out the screen. Use in combination of a tileset switch!";
-            this.tre_tred_transition_tree.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
-            treeNode1});
             this.tre_tred_transition_tree.ShowNodeToolTips = true;
-            this.tre_tred_transition_tree.Size = new System.Drawing.Size(688, 387);
+            this.tre_tred_transition_tree.Size = new System.Drawing.Size(307, 387);
             this.tre_tred_transition_tree.TabIndex = 0;
+            this.tre_tred_transition_tree.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.tre_tred_transition_tree_AfterSelect);
+            // 
+            // grp_tred_edits
+            // 
+            this.grp_tred_edits.Location = new System.Drawing.Point(13, 78);
+            this.grp_tred_edits.Name = "grp_tred_edits";
+            this.grp_tred_edits.Size = new System.Drawing.Size(125, 350);
+            this.grp_tred_edits.TabIndex = 2;
+            this.grp_tred_edits.TabStop = false;
+            this.grp_tred_edits.Text = "Edit";
+            // 
+            // sts_tred_status_strip
+            // 
+            this.sts_tred_status_strip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lbl_tred_transition_length});
+            this.sts_tred_status_strip.Location = new System.Drawing.Point(0, 560);
+            this.sts_tred_status_strip.Name = "sts_tred_status_strip";
+            this.sts_tred_status_strip.Size = new System.Drawing.Size(468, 22);
+            this.sts_tred_status_strip.TabIndex = 3;
+            this.sts_tred_status_strip.Text = "statusStrip1";
+            // 
+            // lbl_tred_transition_length
+            // 
+            this.lbl_tred_transition_length.Name = "lbl_tred_transition_length";
+            this.lbl_tred_transition_length.Size = new System.Drawing.Size(104, 17);
+            this.lbl_tred_transition_length.Text = "Transition Length: ";
+            // 
+            // txt_tred_transition_data
+            // 
+            this.txt_tred_transition_data.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_tred_transition_data.Location = new System.Drawing.Point(7, 22);
+            this.txt_tred_transition_data.Multiline = true;
+            this.txt_tred_transition_data.Name = "txt_tred_transition_data";
+            this.txt_tred_transition_data.ReadOnly = true;
+            this.txt_tred_transition_data.Size = new System.Drawing.Size(439, 94);
+            this.txt_tred_transition_data.TabIndex = 4;
+            // 
+            // grp_tred_raw_data
+            // 
+            this.grp_tred_raw_data.Controls.Add(this.txt_tred_transition_data);
+            this.grp_tred_raw_data.Location = new System.Drawing.Point(12, 435);
+            this.grp_tred_raw_data.Name = "grp_tred_raw_data";
+            this.grp_tred_raw_data.Size = new System.Drawing.Size(452, 122);
+            this.grp_tred_raw_data.TabIndex = 4;
+            this.grp_tred_raw_data.TabStop = false;
+            this.grp_tred_raw_data.Text = "Raw data";
             // 
             // TransitionsEditor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(857, 480);
+            this.ClientSize = new System.Drawing.Size(468, 582);
+            this.Controls.Add(this.grp_tred_raw_data);
+            this.Controls.Add(this.sts_tred_status_strip);
+            this.Controls.Add(this.grp_tred_edits);
             this.Controls.Add(this.grp_tred_transition);
             this.Controls.Add(this.grp_tred_selection);
             this.MaximizeBox = false;
@@ -104,7 +154,12 @@ namespace YAM2E.FORMS
             this.grp_tred_selection.ResumeLayout(false);
             this.grp_tred_selection.PerformLayout();
             this.grp_tred_transition.ResumeLayout(false);
+            this.sts_tred_status_strip.ResumeLayout(false);
+            this.sts_tred_status_strip.PerformLayout();
+            this.grp_tred_raw_data.ResumeLayout(false);
+            this.grp_tred_raw_data.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -115,5 +170,10 @@ namespace YAM2E.FORMS
         private System.Windows.Forms.ComboBox cbb_tred_transition_selection;
         private System.Windows.Forms.GroupBox grp_tred_transition;
         private System.Windows.Forms.TreeView tre_tred_transition_tree;
+        private System.Windows.Forms.GroupBox grp_tred_edits;
+        private System.Windows.Forms.StatusStrip sts_tred_status_strip;
+        private System.Windows.Forms.ToolStripStatusLabel lbl_tred_transition_length;
+        private System.Windows.Forms.TextBox txt_tred_transition_data;
+        private System.Windows.Forms.GroupBox grp_tred_raw_data;
     }
 }
--- YAM2E/FORMS/TransitionsEditor.cs
+++ YAM2E/FORMS/TransitionsEditor.cs
@@ -7,11 +7,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using YAM2E.Classes;
+using YAM2E.FORMS;
 
 namespace YAM2E.FORMS
 {
     public partial class TransitionsEditor : Form
     {
+        //Editor Vars
+        int TableOffset = 0x142E5;
+        int BankOffset = 0x14000;
+        int CurrentTransitionOfsset;
+        List<byte> Transition = new List<byte>();
+
         public TransitionsEditor()
         {
             InitializeComponent();
@@ -21,6 +29,188 @@ namespace YAM2E.FORMS
                 cbb_tred_transition_selection.Items.Add(i.ToString("X3"));
             }
             cbb_tred_transition_selection.SelectedIndex = 0;
+        }
+
+        void LoadTransition(int transition)
+        {
+            //offset for the transition
+            int offset = Editor.ROM[TableOffset + (2 * transition)];
+            offset += (Editor.ROM[TableOffset + (2 * transition) + 1]) << 8;
+            offset += BankOffset - 0x4000;
+            CurrentTransitionOfsset = offset;
+            string DataText = "";
+
+            //loading Transition
+            Transition.Clear();
+            for (int i = 0; i < 64; i++)
+            {
+                byte val = Editor.ROM[offset + i];
+                Transition.Add(val);
+                DataText += " " + val.ToString("X");
+                if (val == 0xFF)
+                {
+                    lbl_tred_transition_length.Text = $"Transition Length: {Transition.Count} bytes";
+                    txt_tred_transition_data.Text = DataText;
+                    return;
+                }
+            }
+            Transition.Clear();
+            lbl_tred_transition_length.Text = $"After 64 bytes (Max Length) no end operant has been found";
+            txt_tred_transition_data.Text = "This is likely not a transition: " + DataText;
+        }
+
+        void ReadTransition()
+        {
+            tre_tred_transition_tree.Nodes.Clear();
+
+            //generating new Tree
+            for (int i = 0; i < Transition.Count; i++)
+            {
+                byte opcode = Transition[i];
+                byte parameter = (byte)(opcode & 0xF);
+                byte parameter2;
+                opcode &= 0xF0;
+
+                TreeNode Command = new TreeNode("");
+
+                //library of opcodes
+                switch (opcode)
+                {
+                    case (0x00): //VRAM / Data Block Transfer
+                        if (parameter == 0x1) Command.Text = "Saves the bank and offset of a BG tileset";
+                        if (parameter == 0x2) Command.Text = "Saves the offset of a sprite tileset";
+                        else Command.Text = "Copies the data";
+                        i++;
+                        parameter = Transition[i++];
+                        Command.Nodes.Add(new TreeNode("From bank: " + parameter.ToString("X")));
+                        parameter = Transition[i++];
+                        parameter2 = Transition[i++];
+                        Command.Nodes.Add(new TreeNode("At offset: " + (parameter + (parameter2 << 8)).ToString("X")));
+                        parameter = Transition[i++];
+                        parameter2 = Transition[i++];
+                        Command.Nodes.Add(new TreeNode("to offset: " + (parameter + (parameter2 << 8)).ToString("X")));
+                        parameter = Transition[i++];
+                        parameter2 = Transition[i];
+                        Command.Nodes.Add(new TreeNode("Data length: " + (parameter + (parameter2 << 8)).ToString("X")));
+                        break;
+
+                    case (0x10): //Select metatile table
+                        Command.Text = "Select metatiles from";
+                        Command.Nodes.Add(new TreeNode("Table: " + parameter.ToString("X")));
+                        Command.
+                        break;
+
+                    case (0x20): //Select collision table
+                        Command.Text = "Select collision from";
+                        Command.Nodes.Add(new TreeNode("Table: " + parameter.ToString("X")));
+                        break;
+
+                    case (0x30): //Select solidity indexes
+                        Command.Text = "Select solidity indices from";
+                        Command.Nodes.Add(new TreeNode("Table: " + parameter.ToString("X")));
+                        break;
+
+                    case (0x40): //Warp
+                        Command.Text = "Warp to";
+                        Command.Nodes.Add(new TreeNode("Area Bank: " + parameter.ToString("X")));
+                        i++;
+                        parameter = Transition[i];
+                        Command.Nodes.Add(new TreeNode("at Screen Y: " + (parameter >> 4).ToString("X")));
+                        Command.Nodes.Add(new TreeNode("and Screen X: " + (parameter & 0xF).ToString("X")));
+                        break;
+
+                    case (0x50): //Retreat from Queen
+                        Command.Text = "Retreat from Queen fight";
+                        break;
+
+                    case (0x60): //Change acid & spike damage
+                        Command.Text = "Change acid & spike damage";
+                        i++;
+                        parameter = Transition[i++];
+                        Command.Nodes.Add(new TreeNode("Acid damage: " + parameter.ToString("X")));
+                        parameter = Transition[i];
+                        Command.Nodes.Add(new TreeNode("Spike damage: " + parameter.ToString("X")));
+                        break;
+
+                    case (0x70): //Exit Queen
+                        Command.Text = "Exit Queen fight";
+                        break;
+
+                    case (0x80): //Enter Queen
+                        Command.Text = "Enter Queen fight";
+                        Command.Nodes.Add(new TreeNode("Bank for current room: " + parameter.ToString("X")));
+                        i++;
+                        parameter = Transition[i++];
+                        parameter2 = Transition[i++];
+                        Command.Nodes.Add(new TreeNode("Scroll Y position: " + (parameter + (parameter2 << 8)).ToString("X")));
+                        parameter = Transition[i++];
+                        parameter2 = Transition[i++];
+                        Command.Nodes.Add(new TreeNode("Scroll X position: " + (parameter + (parameter2 << 8)).ToString("X")));
+                        parameter = Transition[i++];
+                        parameter2 = Transition[i++];
+                        Command.Nodes.Add(new TreeNode("Samus Y position: " + (parameter + (parameter2 << 8)).ToString("X")));
+                        parameter = Transition[i++];
+                        parameter2 = Transition[i];
+                        Command.Nodes.Add(new TreeNode("Samus X position: " + (parameter + (parameter2 << 8)).ToString("X")));
+                        break;
+
+                    case (0x90): //Conditional Transition
+                        Command.Text = "Go to a different Transition if";
+                        i++;
+                        parameter = Transition[i++];
+                        Command.Nodes.Add(new TreeNode("Metroids alive <= " + parameter.ToString("X")));
+                        parameter = Transition[i++];
+                        parameter2 = Transition[i];
+                        Command.Nodes.Add(new TreeNode("New Transition offset: " + (parameter + (parameter2 << 8)).ToString("X")));
+                        break;
+
+                    case (0xA0): //Fadeout
+                        Command.Text = "Fadeout";
+                        break;
+
+                    case (0xB0): //Load Graphics page
+                        if (parameter == 0x1) Command.Text = "Load Background graphics page from";
+                        if (parameter == 0x2) Command.Text = "Load Sprite graphics page from";
+                        i++;
+                        parameter = Transition[i++];
+                        Command.Nodes.Add(new TreeNode("Bank: " + parameter.ToString("X")));
+                        parameter = Transition[i++];
+                        parameter2 = Transition[i];
+                        Command.Nodes.Add(new TreeNode("At offset: " + (parameter + (parameter2 << 8)).ToString("X")));
+                        break;
+
+                    case (0xC0): //Change Music
+                        Command.Text = "Change music to";
+                        Command.Nodes.Add(new TreeNode("Number: " + parameter.ToString("X")));
+                        break;
+
+                    case (0xD0): //Load Message / Special Graphics
+                        Command.Text = "Change special graphics to";
+                        Command.Nodes.Add(new TreeNode("Number: " + parameter.ToString("X")));
+                        break;
+
+                    case (0xF0): //Exit Transition
+                        if (parameter == 0xF) Command.Text = "END TRANSITION";
+                        else Command.Text = "! i n v a l i d !";
+                        break;
+
+                    default:
+                        Command.Text = $"{opcode + parameter}";
+                        break;
+                }
+                tre_tred_transition_tree.Nodes.Add(Command);
+            }
+            tre_tred_transition_tree.ExpandAll();
+        }
+
+        private void tre_tred_transition_tree_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+        }
+
+        private void cbb_tred_transition_selection_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LoadTransition(cbb_tred_transition_selection.SelectedIndex);
+            ReadTransition();
         }
     }
 }
