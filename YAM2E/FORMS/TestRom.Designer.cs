c2512fc776e8c87a51101276432a7cfa098aa73e
ConConner <egal3d@gmail.com>
2022-05-05 22:06:25 +0200

    TestRom form
    Started TestRom form. Unfinished.
    No logic yet either.

---
+++ YAM2E/FORMS/TestRom.Designer.cs
@@ -0,0 +1,346 @@
+﻿namespace YAM2E.FORMS
+{
+    partial class TestRom
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TestRom));
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.grp_tsrm_samus_data = new System.Windows.Forms.GroupBox();
+            this.chb_tsrm_samus_current = new System.Windows.Forms.CheckBox();
+            this.grp_tsrm_samus_screen = new System.Windows.Forms.GroupBox();
+            this.lbl_tsrm_samus_screen_x = new System.Windows.Forms.Label();
+            this.num_tsrm_samus_screen_x = new System.Windows.Forms.NumericUpDown();
+            this.lbl_tsrm_samus_screen_y = new System.Windows.Forms.Label();
+            this.num_tsrm_samus_screen_y = new System.Windows.Forms.NumericUpDown();
+            this.grp_tsrm_samus_position = new System.Windows.Forms.GroupBox();
+            this.lbl_tsrm_samus_position_x = new System.Windows.Forms.Label();
+            this.num_tsrm_samus_position_x = new System.Windows.Forms.NumericUpDown();
+            this.lbl_tsrm_samus_position_y = new System.Windows.Forms.Label();
+            this.num_tsrm_samus_position_y = new System.Windows.Forms.NumericUpDown();
+            this.grp_tsrm_items = new System.Windows.Forms.GroupBox();
+            this.btn_items_bombs = new System.Windows.Forms.Button();
+            this.btn_item_hi_jump = new System.Windows.Forms.Button();
+            this.btn_item_screw_attack = new System.Windows.Forms.Button();
+            this.btn_item_space_jump = new System.Windows.Forms.Button();
+            this.btn_item_spring_ball = new System.Windows.Forms.Button();
+            this.btn_item_spider_ball = new System.Windows.Forms.Button();
+            this.btn_item_varia = new System.Windows.Forms.Button();
+            this.grp_tsrm_samus_data.SuspendLayout();
+            this.grp_tsrm_samus_screen.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.num_tsrm_samus_screen_x)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num_tsrm_samus_screen_y)).BeginInit();
+            this.grp_tsrm_samus_position.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.num_tsrm_samus_position_x)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num_tsrm_samus_position_y)).BeginInit();
+            this.grp_tsrm_items.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(662, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // grp_tsrm_samus_data
+            // 
+            this.grp_tsrm_samus_data.Controls.Add(this.grp_tsrm_items);
+            this.grp_tsrm_samus_data.Controls.Add(this.grp_tsrm_samus_position);
+            this.grp_tsrm_samus_data.Controls.Add(this.grp_tsrm_samus_screen);
+            this.grp_tsrm_samus_data.Controls.Add(this.chb_tsrm_samus_current);
+            this.grp_tsrm_samus_data.Location = new System.Drawing.Point(12, 12);
+            this.grp_tsrm_samus_data.Name = "grp_tsrm_samus_data";
+            this.grp_tsrm_samus_data.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.grp_tsrm_samus_data.Size = new System.Drawing.Size(199, 362);
+            this.grp_tsrm_samus_data.TabIndex = 1;
+            this.grp_tsrm_samus_data.TabStop = false;
+            this.grp_tsrm_samus_data.Text = "Samus";
+            // 
+            // chb_tsrm_samus_current
+            // 
+            this.chb_tsrm_samus_current.AutoSize = true;
+            this.chb_tsrm_samus_current.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.chb_tsrm_samus_current.Location = new System.Drawing.Point(6, 22);
+            this.chb_tsrm_samus_current.Name = "chb_tsrm_samus_current";
+            this.chb_tsrm_samus_current.Size = new System.Drawing.Size(88, 19);
+            this.chb_tsrm_samus_current.TabIndex = 0;
+            this.chb_tsrm_samus_current.Text = "Use Current";
+            this.chb_tsrm_samus_current.UseVisualStyleBackColor = true;
+            // 
+            // grp_tsrm_samus_screen
+            // 
+            this.grp_tsrm_samus_screen.Controls.Add(this.num_tsrm_samus_screen_y);
+            this.grp_tsrm_samus_screen.Controls.Add(this.lbl_tsrm_samus_screen_y);
+            this.grp_tsrm_samus_screen.Controls.Add(this.num_tsrm_samus_screen_x);
+            this.grp_tsrm_samus_screen.Controls.Add(this.lbl_tsrm_samus_screen_x);
+            this.grp_tsrm_samus_screen.Location = new System.Drawing.Point(6, 47);
+            this.grp_tsrm_samus_screen.Name = "grp_tsrm_samus_screen";
+            this.grp_tsrm_samus_screen.Size = new System.Drawing.Size(187, 61);
+            this.grp_tsrm_samus_screen.TabIndex = 1;
+            this.grp_tsrm_samus_screen.TabStop = false;
+            this.grp_tsrm_samus_screen.Text = "Screen";
+            // 
+            // lbl_tsrm_samus_screen_x
+            // 
+            this.lbl_tsrm_samus_screen_x.AutoSize = true;
+            this.lbl_tsrm_samus_screen_x.Location = new System.Drawing.Point(6, 24);
+            this.lbl_tsrm_samus_screen_x.Name = "lbl_tsrm_samus_screen_x";
+            this.lbl_tsrm_samus_screen_x.Size = new System.Drawing.Size(17, 15);
+            this.lbl_tsrm_samus_screen_x.TabIndex = 0;
+            this.lbl_tsrm_samus_screen_x.Text = "X:";
+            // 
+            // num_tsrm_samus_screen_x
+            // 
+            this.num_tsrm_samus_screen_x.Hexadecimal = true;
+            this.num_tsrm_samus_screen_x.Location = new System.Drawing.Point(29, 22);
+            this.num_tsrm_samus_screen_x.Maximum = new decimal(new int[] {
+            15,
+            0,
+            0,
+            0});
+            this.num_tsrm_samus_screen_x.Name = "num_tsrm_samus_screen_x";
+            this.num_tsrm_samus_screen_x.Size = new System.Drawing.Size(59, 23);
+            this.num_tsrm_samus_screen_x.TabIndex = 1;
+            // 
+            // lbl_tsrm_samus_screen_y
+            // 
+            this.lbl_tsrm_samus_screen_y.AutoSize = true;
+            this.lbl_tsrm_samus_screen_y.Location = new System.Drawing.Point(94, 24);
+            this.lbl_tsrm_samus_screen_y.Name = "lbl_tsrm_samus_screen_y";
+            this.lbl_tsrm_samus_screen_y.Size = new System.Drawing.Size(17, 15);
+            this.lbl_tsrm_samus_screen_y.TabIndex = 2;
+            this.lbl_tsrm_samus_screen_y.Text = "Y:";
+            // 
+            // num_tsrm_samus_screen_y
+            // 
+            this.num_tsrm_samus_screen_y.Hexadecimal = true;
+            this.num_tsrm_samus_screen_y.Location = new System.Drawing.Point(117, 22);
+            this.num_tsrm_samus_screen_y.Maximum = new decimal(new int[] {
+            15,
+            0,
+            0,
+            0});
+            this.num_tsrm_samus_screen_y.Name = "num_tsrm_samus_screen_y";
+            this.num_tsrm_samus_screen_y.Size = new System.Drawing.Size(59, 23);
+            this.num_tsrm_samus_screen_y.TabIndex = 3;
+            // 
+            // grp_tsrm_samus_position
+            // 
+            this.grp_tsrm_samus_position.Controls.Add(this.num_tsrm_samus_position_y);
+            this.grp_tsrm_samus_position.Controls.Add(this.lbl_tsrm_samus_position_y);
+            this.grp_tsrm_samus_position.Controls.Add(this.num_tsrm_samus_position_x);
+            this.grp_tsrm_samus_position.Controls.Add(this.lbl_tsrm_samus_position_x);
+            this.grp_tsrm_samus_position.Location = new System.Drawing.Point(6, 114);
+            this.grp_tsrm_samus_position.Name = "grp_tsrm_samus_position";
+            this.grp_tsrm_samus_position.Size = new System.Drawing.Size(187, 61);
+            this.grp_tsrm_samus_position.TabIndex = 1;
+            this.grp_tsrm_samus_position.TabStop = false;
+            this.grp_tsrm_samus_position.Text = "Position";
+            // 
+            // lbl_tsrm_samus_position_x
+            // 
+            this.lbl_tsrm_samus_position_x.AutoSize = true;
+            this.lbl_tsrm_samus_position_x.Location = new System.Drawing.Point(6, 24);
+            this.lbl_tsrm_samus_position_x.Name = "lbl_tsrm_samus_position_x";
+            this.lbl_tsrm_samus_position_x.Size = new System.Drawing.Size(17, 15);
+            this.lbl_tsrm_samus_position_x.TabIndex = 0;
+            this.lbl_tsrm_samus_position_x.Text = "X:";
+            // 
+            // num_tsrm_samus_position_x
+            // 
+            this.num_tsrm_samus_position_x.Hexadecimal = true;
+            this.num_tsrm_samus_position_x.Location = new System.Drawing.Point(29, 22);
+            this.num_tsrm_samus_position_x.Maximum = new decimal(new int[] {
+            15,
+            0,
+            0,
+            0});
+            this.num_tsrm_samus_position_x.Name = "num_tsrm_samus_position_x";
+            this.num_tsrm_samus_position_x.Size = new System.Drawing.Size(59, 23);
+            this.num_tsrm_samus_position_x.TabIndex = 1;
+            // 
+            // lbl_tsrm_samus_position_y
+            // 
+            this.lbl_tsrm_samus_position_y.AutoSize = true;
+            this.lbl_tsrm_samus_position_y.Location = new System.Drawing.Point(94, 24);
+            this.lbl_tsrm_samus_position_y.Name = "lbl_tsrm_samus_position_y";
+            this.lbl_tsrm_samus_position_y.Size = new System.Drawing.Size(17, 15);
+            this.lbl_tsrm_samus_position_y.TabIndex = 2;
+            this.lbl_tsrm_samus_position_y.Text = "Y:";
+            // 
+            // num_tsrm_samus_position_y
+            // 
+            this.num_tsrm_samus_position_y.Hexadecimal = true;
+            this.num_tsrm_samus_position_y.Location = new System.Drawing.Point(117, 22);
+            this.num_tsrm_samus_position_y.Maximum = new decimal(new int[] {
+            15,
+            0,
+            0,
+            0});
+            this.num_tsrm_samus_position_y.Name = "num_tsrm_samus_position_y";
+            this.num_tsrm_samus_position_y.Size = new System.Drawing.Size(59, 23);
+            this.num_tsrm_samus_position_y.TabIndex = 3;
+            // 
+            // grp_tsrm_items
+            // 
+            this.grp_tsrm_items.Controls.Add(this.btn_item_varia);
+            this.grp_tsrm_items.Controls.Add(this.btn_item_spider_ball);
+            this.grp_tsrm_items.Controls.Add(this.btn_item_spring_ball);
+            this.grp_tsrm_items.Controls.Add(this.btn_item_space_jump);
+            this.grp_tsrm_items.Controls.Add(this.btn_item_screw_attack);
+            this.grp_tsrm_items.Controls.Add(this.btn_item_hi_jump);
+            this.grp_tsrm_items.Controls.Add(this.btn_items_bombs);
+            this.grp_tsrm_items.Location = new System.Drawing.Point(6, 181);
+            this.grp_tsrm_items.Name = "grp_tsrm_items";
+            this.grp_tsrm_items.Size = new System.Drawing.Size(187, 175);
+            this.grp_tsrm_items.TabIndex = 2;
+            this.grp_tsrm_items.TabStop = false;
+            this.grp_tsrm_items.Text = "Items";
+            // 
+            // btn_items_bombs
+            // 
+            this.btn_items_bombs.BackColor = System.Drawing.SystemColors.Control;
+            this.btn_items_bombs.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_items_bombs.Image = ((System.Drawing.Image)(resources.GetObject("btn_items_bombs.Image")));
+            this.btn_items_bombs.Location = new System.Drawing.Point(6, 22);
+            this.btn_items_bombs.Name = "btn_items_bombs";
+            this.btn_items_bombs.Size = new System.Drawing.Size(23, 23);
+            this.btn_items_bombs.TabIndex = 0;
+            this.btn_items_bombs.UseVisualStyleBackColor = false;
+            // 
+            // btn_item_hi_jump
+            // 
+            this.btn_item_hi_jump.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_item_hi_jump.Image = ((System.Drawing.Image)(resources.GetObject("btn_item_hi_jump.Image")));
+            this.btn_item_hi_jump.Location = new System.Drawing.Point(6, 51);
+            this.btn_item_hi_jump.Name = "btn_item_hi_jump";
+            this.btn_item_hi_jump.Size = new System.Drawing.Size(23, 23);
+            this.btn_item_hi_jump.TabIndex = 1;
+            this.btn_item_hi_jump.UseVisualStyleBackColor = true;
+            // 
+            // btn_item_screw_attack
+            // 
+            this.btn_item_screw_attack.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_item_screw_attack.Image = ((System.Drawing.Image)(resources.GetObject("btn_item_screw_attack.Image")));
+            this.btn_item_screw_attack.Location = new System.Drawing.Point(93, 39);
+            this.btn_item_screw_attack.Name = "btn_item_screw_attack";
+            this.btn_item_screw_attack.Size = new System.Drawing.Size(23, 23);
+            this.btn_item_screw_attack.TabIndex = 2;
+            this.btn_item_screw_attack.UseVisualStyleBackColor = true;
+            // 
+            // btn_item_space_jump
+            // 
+            this.btn_item_space_jump.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_item_space_jump.Image = ((System.Drawing.Image)(resources.GetObject("btn_item_space_jump.Image")));
+            this.btn_item_space_jump.Location = new System.Drawing.Point(64, 51);
+            this.btn_item_space_jump.Name = "btn_item_space_jump";
+            this.btn_item_space_jump.Size = new System.Drawing.Size(23, 23);
+            this.btn_item_space_jump.TabIndex = 3;
+            this.btn_item_space_jump.UseVisualStyleBackColor = true;
+            // 
+            // btn_item_spring_ball
+            // 
+            this.btn_item_spring_ball.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_item_spring_ball.Image = ((System.Drawing.Image)(resources.GetObject("btn_item_spring_ball.Image")));
+            this.btn_item_spring_ball.Location = new System.Drawing.Point(64, 22);
+            this.btn_item_spring_ball.Name = "btn_item_spring_ball";
+            this.btn_item_spring_ball.Size = new System.Drawing.Size(23, 23);
+            this.btn_item_spring_ball.TabIndex = 4;
+            this.btn_item_spring_ball.UseVisualStyleBackColor = true;
+            // 
+            // btn_item_spider_ball
+            // 
+            this.btn_item_spider_ball.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_item_spider_ball.Image = ((System.Drawing.Image)(resources.GetObject("btn_item_spider_ball.Image")));
+            this.btn_item_spider_ball.Location = new System.Drawing.Point(35, 22);
+            this.btn_item_spider_ball.Name = "btn_item_spider_ball";
+            this.btn_item_spider_ball.Size = new System.Drawing.Size(23, 23);
+            this.btn_item_spider_ball.TabIndex = 5;
+            this.btn_item_spider_ball.UseVisualStyleBackColor = true;
+            // 
+            // btn_item_varia
+            // 
+            this.btn_item_varia.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_item_varia.Image = ((System.Drawing.Image)(resources.GetObject("btn_item_varia.Image")));
+            this.btn_item_varia.Location = new System.Drawing.Point(35, 51);
+            this.btn_item_varia.Name = "btn_item_varia";
+            this.btn_item_varia.Size = new System.Drawing.Size(23, 23);
+            this.btn_item_varia.TabIndex = 6;
+            this.btn_item_varia.UseVisualStyleBackColor = true;
+            // 
+            // TestRom
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(662, 450);
+            this.Controls.Add(this.grp_tsrm_samus_data);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "TestRom";
+            this.Text = "Test Settings";
+            this.grp_tsrm_samus_data.ResumeLayout(false);
+            this.grp_tsrm_samus_data.PerformLayout();
+            this.grp_tsrm_samus_screen.ResumeLayout(false);
+            this.grp_tsrm_samus_screen.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.num_tsrm_samus_screen_x)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num_tsrm_samus_screen_y)).EndInit();
+            this.grp_tsrm_samus_position.ResumeLayout(false);
+            this.grp_tsrm_samus_position.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.num_tsrm_samus_position_x)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num_tsrm_samus_position_y)).EndInit();
+            this.grp_tsrm_items.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.GroupBox grp_tsrm_samus_data;
+        private System.Windows.Forms.CheckBox chb_tsrm_samus_current;
+        private System.Windows.Forms.GroupBox grp_tsrm_items;
+        private System.Windows.Forms.Button btn_item_varia;
+        private System.Windows.Forms.Button btn_item_spider_ball;
+        private System.Windows.Forms.Button btn_item_spring_ball;
+        private System.Windows.Forms.Button btn_item_space_jump;
+        private System.Windows.Forms.Button btn_item_screw_attack;
+        private System.Windows.Forms.Button btn_item_hi_jump;
+        private System.Windows.Forms.Button btn_items_bombs;
+        private System.Windows.Forms.GroupBox grp_tsrm_samus_position;
+        private System.Windows.Forms.NumericUpDown num_tsrm_samus_position_y;
+        private System.Windows.Forms.Label lbl_tsrm_samus_position_y;
+        private System.Windows.Forms.NumericUpDown num_tsrm_samus_position_x;
+        private System.Windows.Forms.Label lbl_tsrm_samus_position_x;
+        private System.Windows.Forms.GroupBox grp_tsrm_samus_screen;
+        private System.Windows.Forms.NumericUpDown num_tsrm_samus_screen_y;
+        private System.Windows.Forms.Label lbl_tsrm_samus_screen_y;
+        private System.Windows.Forms.NumericUpDown num_tsrm_samus_screen_x;
+        private System.Windows.Forms.Label lbl_tsrm_samus_screen_x;
+    }
+}
