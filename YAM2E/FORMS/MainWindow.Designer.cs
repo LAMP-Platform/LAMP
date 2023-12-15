9e5140679bb868c69a6011cd619e1fe3dbd14c60
ConConner <egal3d@gmail.com>
2023-12-15 21:13:50 +0100

    fix: fixed capitalisation which resulted in error

--- YAM2E/FORMS/MainWindow.Designer.cs
+++ YAM2E/FORMS/MainWindow.Designer.cs
@@ -432,7 +432,7 @@ namespace LAMP
             btn_area_clear.Name = "btn_area_clear";
             btn_area_clear.Size = new System.Drawing.Size(180, 22);
             btn_area_clear.Text = "Clear";
-            btn_area_clear.Click += btn_area_clear_Click;
+            btn_area_clear.Click += Btn_Area_Replace_Click;
             // 
             // tool_strip_help
             // 
